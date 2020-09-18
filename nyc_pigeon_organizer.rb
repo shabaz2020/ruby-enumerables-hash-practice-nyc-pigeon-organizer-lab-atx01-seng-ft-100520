3c7e2a74d3214fce6403583d730b7fd9eba5be96
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 20:03:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object({}) do | (key, value), final_array|
+  final_results = data.each_with_object({}) do | (key, value), final_array|
     #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
@@ -10,9 +10,12 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
       end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key)
     end
   end
-    final_array
   end 
   binding.pry
 end
