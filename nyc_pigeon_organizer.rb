ec1274af75c193eae5c09afa756bae667d0099bb
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 19:42:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   hash.each_with_object({}) do | (key, value), final_array|
-    final_array[key] = value + 3
+    binding.pry
+    final_array
   end 
   binding.pry
 end
