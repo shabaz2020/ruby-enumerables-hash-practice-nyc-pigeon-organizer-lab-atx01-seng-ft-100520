d581d32e7faf8a50f984a31b9e128fba1fd94c22
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 19:48:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,9 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key, names|
       binding.pry
+      names.each do |name|
     end
+  end
     final_array
   end 
   binding.pry
